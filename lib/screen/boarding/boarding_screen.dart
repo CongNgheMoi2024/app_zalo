f30108f119f55223d8b146aa127d4a9b7d72e3cf
NguyenDucChien <cnchien@gmail.com>
2024-01-22 22:44:06 +0700

    onboarding
    onboarding

--- lib/screen/boarding/boarding_screen.dart
+++ lib/screen/boarding/boarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:app_zalo/constants/index.dart';
 import 'package:app_zalo/screen/boarding/onboarding1.dart';
 import 'package:app_zalo/screen/boarding/onboarding2.dart';
 import 'package:app_zalo/screen/boarding/onboarding3.dart';
+import 'package:app_zalo/widget/button/button_bottom_navigated.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -46,7 +47,7 @@ class _BoardingScreenState extends State<BoardingScreen> {
             ),
           ),
           Container(
-            alignment: const Alignment(0, 0.6),
+            alignment: const Alignment(0, 0.9),
             child: SmoothPageIndicator(
               controller: _pageController,
               count: 3,
@@ -61,7 +62,7 @@ class _BoardingScreenState extends State<BoardingScreen> {
             ),
           ),
           Container(
-            alignment: Alignment(0, -0.73),
+            alignment: const Alignment(0, -0.73),
             child: SizedBox(
               height: 100.sp,
               child: Text("Zalo",
@@ -72,6 +73,49 @@ class _BoardingScreenState extends State<BoardingScreen> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: SizedBox(
+        height: height * 0.22,
+        width: width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              child: ButtonBottomNavigated(
+                title: "Đăng nhập",
+                onPressed: () {
+                  // Navigator.pushNamed(context, loginScreen);
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              child: SizedBox(
+                  height: 63.sp,
+                  width: width,
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            left: 55.sp, right: 55.sp, bottom: 5.sp, top: 8.sp),
+                        width: width - 40.sp,
+                        decoration: BoxDecoration(
+                            color: greyIcBot.withOpacity(0.3),
+                            borderRadius: BorderRadius.circular(28.sp)),
+                        child: Center(
+                          child: Text(
+                            "Đăng kí",
+                            style: text15.medium.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
