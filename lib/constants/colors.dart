741b9705bf9e61d2348c05182746aeac6274ddcd
NguyenDucChien <cnchien@gmail.com>
2024-01-26 01:55:23 +0700

    REGISTER
    REGISTER

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 const backgroundColor = Color.fromARGB(255, 37, 38, 39);
 
 const bgColor = Color(0xffeaeef3);
-const primaryColor = Color(0xff594eb6);
+const primaryColor = Color(0xff1d2228);
 const errorColor = Color.fromARGB(255, 255, 7, 28);
 const priOrange = Color(0xfffef6e7);
 
-const greyIcBot = Color.fromARGB(255, 151, 140, 255);
+const greyIcBot = Color.fromARGB(156, 29, 34, 40);
 
 const greyIcTop = Color.fromARGB(255, 83, 83, 83);
 
