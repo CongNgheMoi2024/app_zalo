d1afe26f257d7d3bc7e9ac84251e8474c8495fc9
NguyenDucChien <cnchien@gmail.com>
2024-02-01 01:31:33 +0700

    home chat
    home chat

--- lib/screens/home_chat/home_chat_screen.dart
+++ lib/screens/home_chat/home_chat_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app_zalo/constants/assets.dart';
+import 'package:app_zalo/constants/icons.dart';
+import 'package:app_zalo/constants/index.dart';
 import 'package:app_zalo/widget/dismiss_keyboard_widget.dart';
-import 'package:app_zalo/widget/header/header_actions_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeChatScreen extends StatefulWidget {
@@ -17,11 +19,60 @@ class _HomeChatScreenState extends State<HomeChatScreen> {
     return DismissKeyboard(
         child: Scaffold(
             body: SafeArea(
-                child: Column(children: [
-      Expanded(
-          child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, children: [])))
-    ]))));
+                child: SingleChildScrollView(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+          SizedBox(height: 40.sp),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 20.sp, right: 20.sp, top: 12.sp, bottom: 12.sp),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(60),
+                  child: ImageAssets.pngAsset(Png.imgUserGirl,
+                      width: 65.sp, height: 65.sp, fit: BoxFit.cover),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Thư Dễ Thương",
+                      style: text16.black.regular,
+                    ),
+                    SizedBox(
+                      height: 3.sp,
+                    ),
+                    SizedBox(
+                      height: 20.sp,
+                      width: width * 0.55,
+                      child: Text(
+                        "Lần theo những dấu vết đánh rơi. Chờ mãi nơi này một cảm giác",
+                        overflow: TextOverflow.ellipsis,
+                        style: text16.textColor.regular,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20.sp),
+                child: Text(
+                  "3 giờ",
+                  style: text15.regular.textColor,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 0.5.sp,
+            width: width,
+            color: grey03.withOpacity(0.5),
+            margin: EdgeInsets.only(left: 105.sp),
+          )
+        ])))));
   }
 }
