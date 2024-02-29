f0cdb9ab7e978f17441d7e09c750c81633e51a3e
NguyenDucChien <cnchien@gmail.com>
2024-02-29 15:34:37 +0700

    Revert "Merge pull request #9 from CongNgheMoi2024/feature_login"
    This reverts commit 88687e9b6b3f8ee53926204671aa2ae49308ca1b, reversing
    changes made to 5a7ea88c49ef2f75c07064ea9c20a50dfabf0e9e.

--- lib/screens/dashboard/ui/dashboard.dart
+++ lib/screens/dashboard/ui/dashboard.dart
@@ -1,4 +1,3 @@
-
 import 'package:app_zalo/constants/index.dart';
 import 'package:app_zalo/screens/fast_contact/fast_contact_screen.dart';
 import 'package:app_zalo/screens/home_account/home_account_screen.dart';
@@ -12,6 +11,7 @@ class DashboardScreen extends StatefulWidget {
   @override
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
+
 List<bool> isSelectedB = [true, false, false, false];
 
 class _DashboardScreenState extends State<DashboardScreen>
